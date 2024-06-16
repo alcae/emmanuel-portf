45ac1c006f022ebcaa5008eb8283cc83eb82d78e
alcae <67080834+alcae@users.noreply.github.com>
2024-06-16 17:23:03 -0400

    Update about.tsx

--- components/about.tsx
+++ components/about.tsx
@@ -15,7 +15,7 @@ export default function About() {
       initial={{ opacity: 0, y: 100 }}
       animate={{ opacity: 1, y: 0 }}
       transition={{ delay: 0.175 }}
-      id="about"
+      id="acerca de"
     >
       <SectionHeading>Acerca de mí</SectionHeading>
       <p className="mb-3">
